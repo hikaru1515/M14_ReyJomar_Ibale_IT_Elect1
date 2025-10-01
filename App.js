aaa0634e82f5bac0db81a1c7bc8f04d883cf066d
Rey jomar <160912869+hikaru1515@users.noreply.github.com>
2025-10-02 00:35:53 +0800

    App.js

--- App.js
+++ App.js
@@ -1,1 +1,22 @@
 
+import React from "react";
+import ChatScreen from "./ChatScreen";
+import CommentSection from "./CommentSection";
+import ColorChanger from "./ColorChanger";
+import CounterApp from "./CounterApp";
+
+export default function App() {
+  return (
+    <>
+       <ChatScreen />,
+      <CommentSection/>,
+      <ColorChanger/>,
+      <CounterApp/>;
+      
+      
+    </>
+    
+    
+    )
+  
+    }
